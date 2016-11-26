72d0ff0d807456ea1107f4155a9eac9986236b60
Ebben Feagan <ebben.feagan@gmail.com>
2016-11-26 11:44:39 -0600

    Added new function to determine more reliably the width and height

--- SiS_Backend/JpgInfo.cs
+++ SiS_Backend/JpgInfo.cs
@@ -73,10 +73,13 @@ namespace SpotlightImageSaver
             {
                 using (MemoryStream ms = new MemoryStream(finfo))
                 {
-                    using (Image img = Image.FromStream(ms))
+                    if (!getJpgDimensions(ms, out width, out height))
                     {
-                        width = img.Size.Width;
-                        height = img.Size.Height;
+                        using (Image img = Image.FromStream(ms))
+                        {
+                            width = img.Size.Width;
+                            height = img.Size.Height;
+                        }
                     }
                 }
             }
@@ -84,6 +87,66 @@ namespace SpotlightImageSaver
             if (height > width) isPortrait = true;
 
             newFileName = Path.GetFileNameWithoutExtension(filepath) + ".spotlight.jpg";
+        }
+
+        private bool getJpgDimensions(Stream buffer, out int width, out int height)
+        {
+            width = height = 0;
+            bool found = false;
+            bool eof = false;
+            using (BinaryReader reader = new BinaryReader(buffer))
+            {
+                while (!found || eof)
+                {
+                    // read 0xFF and the type
+                    reader.ReadByte();
+                    byte type = reader.ReadByte();
+
+                    // get length
+                    int len = 0;
+                    switch (type)
+                    {
+                        // start and end of the image
+                        case 0xD8:
+                        case 0xD9:
+                            len = 0;
+                            break;
+
+                        // restart interval
+                        case 0xDD:
+                            len = 2;
+                            break;
+
+                        // the next two bytes is the length
+                        default:
+                            int lenHi = reader.ReadByte();
+                            int lenLo = reader.ReadByte();
+                            len = (lenHi << 8 | lenLo) - 2;
+                            break;
+                    }
+
+                    // EOF?
+                    if (type == 0xD9)
+                        eof = true;
+
+                    // process the data
+                    if (len > 0)
+                    {
+                        // read the data
+                        byte[] data = reader.ReadBytes(len);
+
+                        // this is what we are looking for
+                        if (type == 0xC0)
+                        {
+                            width = data[1] << 8 | data[2];
+                            height = data[3] << 8 | data[4];
+                            found = true;
+                        }
+                    }
+                }
+            }
+
+            return found;
         }
     }
 }
